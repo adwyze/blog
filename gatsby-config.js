0ff7623370a814cbeb22e500b89c92c56fc6abdf
Nishant Arora <na.nishantarora@gmail.com>
2018-01-10 17:48:12 +0000

    Basic blog works

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `The Adwyze Blog`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-catch-links`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-catch-links`, {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      path: `${__dirname}/src/posts`,
+      name: 'pages',
+    },
+  }, {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [] // just in case those previously mentioned remark plugins sound cool :)
+    }
+  },],
 }
