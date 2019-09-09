23b539f58ada99352a4608cccf92bb9021c9589d
Yash Arora <arorayash.work@gmail.com>
2019-09-09 13:47:02 +0530

    fixed container padding for mobile

--- src/ui/PostContainer.js
+++ src/ui/PostContainer.js
@@ -3,6 +3,10 @@ import styled from "styled-components"
 const PostContainer = styled.div`
   margin-top: 80px;
   padding: 0 10vh;
+
+  @media (max-width: 768px) {
+    padding: 0 10px;
+  }
 `
 
 export default PostContainer
